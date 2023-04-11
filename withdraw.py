5fbc5cbeeacb61ee332e41176b0d9dbb0e0a9c9b
arnolgto <54421413+arnolgto@users.noreply.github.com>
2023-04-10 19:09:52 -0600

    Add files via upload

--- withdraw.py
+++ withdraw.py
@@ -6,7 +6,6 @@ import requests
 import json
 import random
 import os
-import datetime
 
 # Reading the value of an environment variable
 API = os.getenv('API')
@@ -39,8 +38,7 @@ def my_task():
         print("Success")
         url = URL
         data = {
-            "amount": str(amount),
-            "servidor": formatted_time
+            "amount": str(amount)
         }
 
         # convert the data to JSON format
